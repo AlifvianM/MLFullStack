821535b906b15e111bac5ace31a1d6f55201b7b7
root <root@LAPTOP-RR133QQC>
2024-01-21 20:41:43 +0700

    Fix migrations alembic

--- src/BE/model/database.py
+++ src/BE/model/database.py
@@ -1,11 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = "postgresql://alifvianmarco:alifvianmaro@localhost:5432/irisdb"
+DATABASE_URL = os.environ["POSTGRES_URL"]
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
 base = declarative_base()
 
